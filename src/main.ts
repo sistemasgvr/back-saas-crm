2f0c141e7304ecb64d67852b84548f449a0a3e20
santossjba <bernuijesus363@gmail.com>
2026-08-17 18:29:52 -0500

    COnfiguracion de acuerdo a hostinger

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.enableCors({ origin: process.env.FRONTEND_URL, credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.PORT ?? 4000);
+  const port = Number(process.env.PORT ?? 4000);
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
