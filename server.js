1747ae0e239ecb00859822e2eb12b09760c96e0e
santossjba <bernuijesus363@gmail.com>
2026-08-17 18:38:37 -0500

    Mejoras

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /**
  * Entry point para Hostinger Node.js Web Apps.
  * hPanel → Entry file: server.js
- * (Nest compila a dist/src/main.js, no dist/main.js)
  */
-require('./dist/src/main.js');
+require('./dist/main.js');
